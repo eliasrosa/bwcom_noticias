fbf88938bde58be30f40891217d01b14799ef472
Elias da Rosa <elias@eliasdarosa.com.br>
2012-07-11 21:52:47 -0300

    Alterado as rotas

--- router.php
+++ router.php
@@ -1,48 +1,8 @@
 <?
 defined('BW') or die("Acesso negado!");
 
-function noticiasBuildRoute( &$query )
-{
-	$segments = array();	
-
-	if(preg_match('#open#', $query['view']))
-	{
-		
-		if(!isset($query['id']) || !isset($query['alias']))
-		{
-			$t = Doctrine_Query::create()
-				->from('Noticia n')
-				->where('n.status = 1')
-				->orderBy("n.datahora DESC, n.titulo ASC")
-				->fetchOne();
-				
-			if($t)
-			{
-				$segments[] = $t->id;
-				$segments[] = bwUtil::alias($t->titulo);		
-			}
-		}
-		else
-		{
-			$segments[] = $query['id'];
-			$segments[] = $query['alias'];
-			unset($query['id'], $query['alias']);
-		}
-	}
-
-	return $segments;
-}
-
-function noticiasParseRoute( $segments )
-{
-	$vars = array();
-	
-	if(count($segments))
-	{
-		$vars['id'] = $segments[0];		
-		$vars['alias'] = $segments[1];		
-	}
-	
-	return $vars;	
-}
+bwRouter::addUrl('/noticias');
+bwRouter::addUrl('/noticias/detalhes/:metatagalias', '/noticias/detalhes', array(
+    ':metatagalias' => '[\w\d-]+'
+));
 ?>
