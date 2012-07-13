452149dc0038ab7305897544d3f31d9ea4b59085
Elias da Rosa <elias@eliasdarosa.com.br>
2012-07-13 00:14:40 -0300

    Atualizado e removido alguns arquivos

--- router.php
+++ router.php
@@ -2,7 +2,5 @@
 defined('BW') or die("Acesso negado!");
 
 bwRouter::addUrl('/noticias');
-bwRouter::addUrl('/noticias/detalhes/:metatagalias', '/noticias/detalhes', array(
-    ':metatagalias' => '[\w\d-]+'
-));
+bwRouter::addUrl('/noticias/detalhes/:alias/:id', array('titulo', 'id'));
 ?>
