205c7adacc41051f59e7697ed27092c94ec513b7
Elias da Rosa <elias@eliasdarosa.com.br>
2012-07-16 17:14:24 -0300

    Adicionado proteção ao remover uma categoria

--- models/NoticiaCategoria.php
+++ models/NoticiaCategoria.php
@@ -2,53 +2,54 @@
 
 class NoticiaCategoria extends bwRecord
 {
+
     var $labels = array(
         'nome' => 'Nome',
         'descricao' => 'Descrição'
     );
-    
+
     public function setTableDefinition()
     {
         $this->setTableName('bw_noticias_categorias');
         $this->hasColumn('id', 'integer', 4, array(
-             'type' => 'integer',
-             'length' => 4,
-             'fixed' => false,
-             'unsigned' => false,
-             'primary' => true,
-             'autoincrement' => true,
-             ));
+            'type' => 'integer',
+            'length' => 4,
+            'fixed' => false,
+            'unsigned' => false,
+            'primary' => true,
+            'autoincrement' => true,
+        ));
         $this->hasColumn('nome', 'string', 255, array(
-             'type' => 'string',
-             'length' => 255,
-             'fixed' => false,
-             'unsigned' => false,
-             'primary' => false,
-             'notnull' => true,
-             'notblank' => true,
-             'unique' => true,
-             'autoincrement' => false,
-             ));
+            'type' => 'string',
+            'length' => 255,
+            'fixed' => false,
+            'unsigned' => false,
+            'primary' => false,
+            'notnull' => true,
+            'notblank' => true,
+            'unique' => true,
+            'autoincrement' => false,
+        ));
         $this->hasColumn('descricao', 'string', null, array(
-             'type' => 'string',
-             'fixed' => false,
-             'unsigned' => false,
-             'primary' => false,
-             'notnull' => false,
-             'autoincrement' => false,
-             ));
+            'type' => 'string',
+            'fixed' => false,
+            'unsigned' => false,
+            'primary' => false,
+            'notnull' => false,
+            'autoincrement' => false,
+        ));
         $this->hasColumn('status', 'integer', 4, array(
-             'type' => 'integer',
-             'length' => 4,
-             'fixed' => false,
-             'unsigned' => false,
-             'primary' => false,
-             'notnull' => true,
-             'autoincrement' => false,
-             ));
+            'type' => 'integer',
+            'length' => 4,
+            'fixed' => false,
+            'unsigned' => false,
+            'primary' => false,
+            'notnull' => true,
+            'autoincrement' => false,
+        ));
     }
 
-   public function setUp()
+    public function setUp()
     {
         parent::setUp();
 
@@ -57,7 +58,7 @@ class NoticiaCategoria extends bwRecord
             'foreign' => 'idcategoria'
         ));
     }
-    
+
     public function salvar($dados)
     {
         $db = bwComponent::save('NoticiaCategoria', $dados);
@@ -68,11 +69,22 @@ class NoticiaCategoria extends bwRecord
 
     public function remover($dados)
     {
+        // verifica se exite notícias relacionadas
+        $dql = Doctrine_Query::create()
+            ->from('Noticia')
+            ->where('idcategoria = ?', $dados['id']);
+
+        if ($dql->fetchOne()) {
+            return array(
+                'retorno' => false,
+                'msg' => 'Existem notícias cadastradas nesta categoria!',
+            );
+        }
+        
         $db = bwComponent::remover('NoticiaCategoria', $dados);
         $r = bwComponent::retorno($db);
 
         return $r;
-    }    
-    
-    
+    }
+
 }
