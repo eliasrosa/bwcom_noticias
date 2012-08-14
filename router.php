7f722b66365a84b5029f5050373a2d0dafea4acd
Elias da Rosa <elias@eliasdarosa.com.br>
2012-08-14 12:32:05 -0300

    Atualizado as rotas

--- router.php
+++ router.php
@@ -3,12 +3,12 @@ defined('BW') or die("Acesso negado!");
 
 // SITE
 bwRouter::addUrl('/noticias');
-bwRouter::addUrl('/noticias/detalhes/:alias/:id', array('titulo', 'id'));
+bwRouter::addUrl('/noticias/detalhes/:alias/:id', array('fields' => array('titulo', 'id')));
 
 // ADM
-bwRouter::addUrl('/noticias/task', array(), 'task');
+bwRouter::addUrl('/noticias/task', array('type' => 'task'));
 bwRouter::addUrl('/noticias/lista');
-bwRouter::addUrl('/noticias/cadastro/:id', array('id'));
+bwRouter::addUrl('/noticias/cadastro/:id', array('fields' => array('id')));
 bwRouter::addUrl('/noticias/categorias/lista');
-bwRouter::addUrl('/noticias/categorias/cadastro/:id', array('id'));
+bwRouter::addUrl('/noticias/categorias/cadastro/:id', array('fields' => array('id')));
 ?>
